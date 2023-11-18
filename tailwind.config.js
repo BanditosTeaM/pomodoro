269ab7e1dd4eaa3af66dea1f219127fe077db5d1
BanditosTM <banditostm123@gmail.com>
2023-11-18 17:51:04 +0300

    Install postcss, created editorconfig, configured eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{vue,js,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
 }
-

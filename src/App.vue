1cb141e5b6881c4f31b42ffae63e43104f5de493
BanditosTM <banditostm123@gmail.com>
2023-12-11 21:40:42 +0300

    Сonnection history panel, adding dark mode, created functions for notification

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,48 @@
 <script setup>
 import { watchEffect, onMounted, ref, computed } from 'vue'
 import { useTimer } from 'vue-timer-hook'
-import ModalSettings from './components/ModalWindowSettings.vue'
+import ModalSettings from './components/PanelSettings.vue'
+import ModalHistory from './components/PanelHistory.vue'
 import TimerDigit from './components/TimerDigit.vue'
 import { useDataStore } from '../src/store'
+import { useDark } from '@vueuse/core'
 
+const isDark = useDark()
 const dataStore = useDataStore()
 
+const isModalHistoryOpen = ref(false)
 const isModalSettingsOpen = ref(false)
 const checkTimer = ref('Start')
 const skipPartTimer = ref(1)
 
 const titleTimerData = [
 	{
-		id: 'work',
-		title: 'Work Time'
+		work: 'Work'
 	},
 	{
-		id: 'shortBreak',
-		title: 'Short Break'
+		shortBreak: 'Short Break'
 	},
 	{
-		id: 'longBreak',
-		title: 'Long Break'
+		longBreak: 'Long Break'
 	}
 ]
-
-const selectedTitle = computed(() => {
+const setTitleTimer = computed(() => {
 	const title = titleTimerData.find(
-		item => item.id === dataStore.settings.selectedMode
-	)
-	return title.title
+		item => Object.keys(item)[0] === dataStore.settings.selectedMode
+	)[dataStore.settings.selectedMode]
+	return title
 })
-
-const BGColorTimer = computed(() => {
-	return dataStore.settings.selectedBGColor
-})
-
 // from minutes to milliseconds
 const workTimeMode = computed(() => {
-	return dataStore.settings.times.work
+	return dataStore.settings.times.work * 1000
 })
 
 const shortBreakTimeMode = computed(() => {
-	return dataStore.settings.times.shortBreak
+	return dataStore.settings.times.shortBreak * 1000
 })
 
 const longBreakTimeMode = computed(() => {
-	return dataStore.settings.times.longBreak
+	return dataStore.settings.times.longBreak * 60 * 1000
 })
 
 const timer = useTimer(Date.now() + workTimeMode.value, false)
@@ -77,6 +72,12 @@ function switchingTimer() {
 
 function toggleModalSetings() {
 	isModalSettingsOpen.value = !isModalSettingsOpen.value
+	isModalHistoryOpen.value = false
+}
+
+function toggleModalHistory() {
+	isModalHistoryOpen.value = !isModalHistoryOpen.value
+	isModalSettingsOpen.value = false
 }
 
 function updatedWorkTime() {
@@ -96,86 +97,163 @@ function updatedLongBreakTime() {
 
 function cycleSkipTimerPart() {
 	if (skipPartTimer.value >= 4) {
-		return (skipPartTimer.value = 1)
+		restartTimer()
+		skipPartTimer.value = 1
+		return
 	}
 
 	skipPartTimer.value += 1
+	restartTimer()
+}
+
+const showNotification = () => {
+	if (dataStore.settings.notificationsEnabled && 'Notification' in window) {
+		if (Notification.permission === 'granted') {
+			createNotification()
+		} else if (Notification.permission !== 'denied') {
+			Notification.requestPermission().then(permission => {
+				if (permission === 'granted') {
+					createNotification()
+				}
+			})
+		}
+	}
+}
+
+const createNotification = () => {
+	const notification = new Notification('Pomodoro', {
+		body: `Time to ${setTitleTimer.value}`,
+		icon: 'путь/к/изображению.png'
+	})
+
+	notification.onclick = () => {
+		console.log('Уведомление было кликнуто.')
+	}
 }
 
 onMounted(() => {
+	dataStore.initializeNotificationsState(), dataStore.initializeHistoryState()
+
 	watchEffect(async () => {
 		if (timer.isExpired.value) {
 			if (dataStore.settings.selectedMode !== 'work') {
-				skipPartTimer.value += 1
+				if (dataStore.settings.selectedMode === 'shortBreak') {
+					dataStore.addHistory(
+						shortBreakTimeMode.value,
+						'Short Break',
+						skipPartTimer.value
+					)
 
-				if (dataStore.settings.selectedMode === 'longBreak') {
+					skipPartTimer.value += 1
+				} else {
+					dataStore.addHistory(
+						longBreakTimeMode.value,
+						'Long Break',
+						skipPartTimer.value
+					)
+
 					skipPartTimer.value = 1
 				}
 
 				dataStore.setSelectedMode('work')
+
+				showNotification()
 				restartTimer()
 				return
 			}
+
+			dataStore.addHistory(workTimeMode.value, 'Work', skipPartTimer.value)
 
 			if (skipPartTimer.value >= 4) {
 				dataStore.setSelectedMode('longBreak')
 			} else {
 				dataStore.setSelectedMode('shortBreak')
 			}
+			showNotification()
 			restartTimer()
 		}
 	})
-	dataStore.initializeBGColor()
 })
 </script>
 
 <template>
-	<main :class="`${BGColorTimer} w-full h-full min-h-screen`">
+	<main
+		class="w-full h-full min-h-screen"
+		:class="{
+			dark: isDark,
+			'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500': !isDark
+		}"
+	>
 		<div
-			class="flex flex-col justify-between absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 text-center bg-white w-96 h-60 rounded-md"
+			class="absolute w-full h-full flex flex-col justify-center items-center"
 		>
-			<div>
-				<h1>{{ selectedTitle }}</h1>
-				<h2>{{ skipPartTimer }} / 4</h2>
+			<h1 class="block -mt-60 text-6xl">{{ setTitleTimer }}</h1>
 
-				<div class="text-lg">
-					<TimerDigit :digit="timer.minutes" />
-					:
-					<TimerDigit :digit="timer.seconds" />
+			<div
+				class="flex flex-col justify-center items-center mt-40 w-[450px] font-semibold"
+			>
+				<div>
+					<div class="-mt-10 mb-10 text-6xl">
+						<TimerDigit :digit="timer.minutes" />
+
+						<TimerDigit
+							class="-mt-5"
+							:digit="timer.seconds"
+						/>
+					</div>
+				</div>
+				<h2 class="mt-4 text-2xl">{{ skipPartTimer }} / 4</h2>
+				<div class="flex mt-10 space-x-24 text-2xl">
+					<button
+						class="w-full"
+						@click="switchingTimer()"
+					>
+						{{ checkTimer }}
+					</button>
+					<button
+						class="w-full"
+						@click="restartTimer()"
+					>
+						Restart
+					</button>
+					<button
+						class="w-full"
+						@click="cycleSkipTimerPart()"
+					>
+						Next
+					</button>
 				</div>
 			</div>
-			<div class="flex h-10">
+		</div>
+		<div class="absolute flex flex-col pt-2">
+			<div
+				class="flex justify-center mt-3 px-1 py-1 border border-black border-solid rounded-r-lg translate-x-0 transition-all duration-500 dark:border-white"
+				:class="{
+					'!translate-x-[424px]': isModalSettingsOpen || isModalHistoryOpen
+				}"
+			>
 				<button
-					class="w-full"
-					@click="switchingTimer()"
+					class="h-7"
+					@click="toggleModalSetings"
 				>
-					{{ checkTimer }}
+					Settings
 				</button>
+			</div>
+			<div
+				class="flex justify-center mt-3 px-1 py-1 border border-black border-solid rounded-r-lg translate-x-0 transition-all duration-500 dark:border-white"
+				:class="{
+					'!translate-x-[424px]': isModalSettingsOpen || isModalHistoryOpen
+				}"
+			>
 				<button
-					class="w-full"
-					@click="restartTimer()"
+					class="h-7"
+					@click="toggleModalHistory"
 				>
-					Restart
-				</button>
-				<button
-					class="w-full"
-					@click="cycleSkipTimerPart()"
-				>
-					Next
+					History
 				</button>
 			</div>
 		</div>
-		<div
-			class="absolute pl-4"
-			:class="{ 'translate-x-[425px]': isModalSettingsOpen === true }"
-		>
-			<button
-				class="text-black pt-2"
-				@click="toggleModalSetings"
-			>
-				Settings
-			</button>
-		</div>
+
 		<ModalSettings
 			v-if="isModalSettingsOpen"
 			@update:worktime="updatedWorkTime"
@@ -183,5 +261,6 @@ onMounted(() => {
 			@update:longbreaktime="updatedLongBreakTime"
 			@update:bgcolor="value => dataStore.updateBGColor(value)"
 		></ModalSettings>
+		<ModalHistory v-if="isModalHistoryOpen"></ModalHistory>
 	</main>
 </template>

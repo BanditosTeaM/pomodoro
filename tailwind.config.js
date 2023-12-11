46d0ba22f98a23957cc75502538d5a1a495109cb
BanditosTM <banditostm123@gmail.com>
2023-12-11 21:29:44 +0300

    Adding new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: 'class',
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				wheat: '#f5deb3'
+			}
+		}
 	},
 	plugins: []
 }

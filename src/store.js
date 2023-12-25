7235e3b804eab40a9680eaf0d27614c7b26e86da
BanditosTM <banditostm123@gmail.com>
2023-12-25 19:28:22 +0300

    Adding title timer data

--- src/store.js
+++ src/store.js
@@ -4,9 +4,13 @@ import storageHistory from './storage/adapters/history'
 
 export const useDataStore = defineStore('data', {
 	state: () => ({
+		titleTimerData: [
+			{ work: 'Work' },
+			{ shortBreak: 'Short Break' },
+			{ longBreak: 'Long Break' }
+		],
 		settings: {
 			times: {
-				// in minutes
 				work: 2,
 				shortBreak: 3,
 				longBreak: 4
@@ -17,7 +21,15 @@ export const useDataStore = defineStore('data', {
 		},
 		history: []
 	}),
-	// XXX: ADD ENTER!!!!!!
+
+	getters: {
+		timerTitle: state => {
+			return state.titleTimerData.find(
+				item => item[state.settings.selectedMode]
+			)[state.settings.selectedMode]
+		}
+	},
+
 	actions: {
 		initializeNotificationsState() {
 			const jsonNotification = JSON.parse(
@@ -27,40 +39,36 @@ export const useDataStore = defineStore('data', {
 
 			this.settings.notificationsEnabled = jsonNotification
 		},
-		// XXX: ADD ENTER!!!!!!
+
 		initializeHistoryState() {
 			const jsonHistory = JSON.parse(storageHistory.getHistoryInStorage())
 			if (!jsonHistory) return
 
 			this.history = jsonHistory
 		},
-		// XXX: ADD ENTER!!!!!!
+
 		setSelectedMode(newMode) {
 			this.settings.selectedMode = newMode
 		},
-		// XXX: ADD ENTER!!!!!!
+
 		updateWorkTime(newWorkTime) {
 			this.settings.times.work = newWorkTime
 		},
-		// XXX: ADD ENTER!!!!!!
+
 		updateShortBreakTime(newShortBreakTime) {
 			this.settings.times.shortBreak = newShortBreakTime
 		},
 		updateLongBreakTime(newLongBreakTime) {
 			this.settings.times.longBreak = newLongBreakTime
 		},
-		// XXX: ADD ENTER!!!!!!
-		updateBGColor(newBGColor) {
-			this.settings.selectedBGColor = newBGColor
-		},
-		// XXX: ADD ENTER!!!!!!
+
 		setNotificationSetting(value) {
 			this.settings.notificationsEnabled = value
 			storageNotification.setNotificationInStorage(
 				this.settings.notificationsEnabled
 			)
 		},
-		// XXX: ADD ENTER!!!!!!
+
 		addHistory(count, mode, partTimer) {
 			this.history.push({
 				id: String(Math.floor(new Date().getTime() / 1000)),
@@ -68,10 +76,10 @@ export const useDataStore = defineStore('data', {
 				mode: mode,
 				partTimer: partTimer
 			})
-			// XXX: ADD ENTER!!!!!!
+
 			storageHistory.setHistoryInStorage(this.history)
 		},
-		// XXX: ADD ENTER!!!!!!
+
 		deleteHistory(id) {
 			this.history = this.history.filter(item => item.id !== id)
 			storageHistory.setHistoryInStorage(this.history)

bb8414ef00628a9f9e77b1c4e0d39782be632b79
BanditosTM <banditostm123@gmail.com>
2024-01-08 01:02:39 +0300

    chore: integrated pinia-plugin-persistedstate to persist and restore application state

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 import './style.css'
 import App from './App.vue'
 
@@ -7,5 +9,6 @@ const app = createApp(App)
 
 const pinia = createPinia()
 app.use(pinia)
+pinia.use(piniaPluginPersistedstate)
 
 app.mount('#app')

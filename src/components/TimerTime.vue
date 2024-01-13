996fec93743bcc7abb7f8ffd42b5beeac591b923
BanditosTM <banditostm123@gmail.com>
2024-01-13 14:53:12 +0300

    feat: move timer time to separate component

---
+++ src/components/TimerTime.vue
@@ -0,0 +1,26 @@
+<script setup>
+import TimerDigit from './TimerDigit.vue'
+
+const props = defineProps({
+	seconds: {
+		type: Number,
+		default: 0
+	},
+	minutes: {
+		type: Number,
+		default: 0
+	}
+})
+</script>
+<template>
+	<div>
+		<div class="-mt-10 mb-10 text-6xl">
+			<TimerDigit :digit="props.minutes" />
+
+			<TimerDigit
+				class="-mt-5"
+				:digit="props.seconds"
+			/>
+		</div>
+	</div>
+</template>

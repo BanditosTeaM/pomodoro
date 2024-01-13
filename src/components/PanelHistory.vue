1bef29a393735947194d7c0755d749471a13b659
BanditosTM <banditostm123@gmail.com>
2024-01-13 15:13:16 +0300

    refactor: improve History component

--- src/components/PanelHistory.vue
+++ src/components/PanelHistory.vue
@@ -1,23 +1,25 @@
 <script setup>
-import { computed, onMounted } from 'vue'
-import { useDataStore } from '../store'
+import { computed } from 'vue'
 import closeIcon from '../assets/close.svg'
-import { sortedById } from '../helpers/sortFuncById'
+import { reverseSortById } from '@helpers/reverseSortById'
+import { useHistoryStore } from '@store/storeHistory'
+import { useSettingsStore } from '@store/storeSettings'
 
-const dataStore = useDataStore()
+const historyStore = useHistoryStore()
+const settingsStore = useSettingsStore()
 
-const sortedHistoryList = computed(() => {
-	return sortedById(dataStore.history)
+const part = computed(() => {
+	return settingsStore.settings.partTimer
 })
 
-onMounted(() => {
-	dataStore.initializeHistoryState()
+const sortedHistoryList = computed(() => {
+	return historyStore.history.toSorted(reverseSortById)
 })
 </script>
 
 <template>
 	<div
-		class="flex flex-col absolute w-[425px] min-h-screen pl-5 pr-5 border-r border-black text-lg font-medium dark:border-white"
+		class="flex flex-col w-[425px] min-h-screen pl-5 pr-5 border-r border-black text-lg font-medium dark:border-white max-md:w-full"
 	>
 		<div class="h-full overflow-y-auto">
 			<h1 class="pb-2 pt-2 font-bold text-xl text-center">History</h1>
@@ -28,13 +30,13 @@ onMounted(() => {
 			>
 				<div class="flex justify-between items-center">
 					<span>Time: {{ item.count }}(min)</span>
-					<button @click="dataStore.deleteHistory(item.id)">
+					<button @click="historyStore.deleteHistory(item.id)">
 						<closeIcon />
 					</button>
 				</div>
 				<div class="flex justify-between items-center">
 					<span>{{ item.mode }}</span>
-					<span>({{ item.partTimer }} / 4)</span>
+					<span>({{ item.partTimer }} / {{ part }})</span>
 				</div>
 				<hr class="min-w-full h-px border-black mb-2 mt-2 dark:border-white" />
 			</div>

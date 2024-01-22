45d6f3c92bdabc5642937439ec5bb8c72d879bf3
BanditosTM <banditostm123@gmail.com>
2024-01-22 17:02:34 +0300

    chore: rename aliases

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 			'@assets': path.resolve(__dirname, './src/assets'),
 			'@components': path.resolve(__dirname, './src/components'),
-			'@composables': path.resolve(__dirname, './src/composables'),
+			'@composable': path.resolve(__dirname, './src/composable'),
 			'@config': path.resolve(__dirname, './src/config'),
 			'@constants': path.resolve(__dirname, './src/constants'),
 			'@helpers': path.resolve(__dirname, './src/helpers'),

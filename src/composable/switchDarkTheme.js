345bd88f86116cc59aa11c27f110dc3220f2ec95
BanditosTM <banditostm123@gmail.com>
2024-01-22 17:09:47 +0300

    refactor: refactor switch theme and add system theme

--- src/composable/switchDarkTheme.js
+++ src/composable/switchDarkTheme.js
@@ -1,12 +1,61 @@
-import { ref } from 'vue'
+import { ref, watch, onMounted } from 'vue'
+import { useSettingsStore } from '@store/storeSettings'
 
 export const switchDarkTheme = () => {
-	const darkMode = ref(false)
+	const settingsStore = useSettingsStore()
+	const darkSchemeMedia = matchMedia('(prefers-color-scheme: dark)')
 
-	const toggleDarkMode = () => {
-		darkMode.value = !darkMode.value
-		document.body.classList.toggle('dark', darkMode.value)
+	const colorTheme = ref(settingsStore.settings.colorTheme)
+
+	function getSystemScheme() {
+		const darkScheme = darkSchemeMedia.matches
+		return darkScheme ? 'dark' : 'light'
 	}
 
-	return { toggleDarkMode, darkMode }
+	const setupSwitcher = () => {
+		const savedScheme = colorTheme.value
+		const currentRadio = document.querySelector(
+			`.switcher_radio[value=${savedScheme}]`
+		)
+
+		if (currentRadio !== null) {
+			currentRadio.checked = true
+		}
+	}
+
+	const setupScheme = () => {
+		const systemScheme = getSystemScheme()
+		const savedScheme = colorTheme.value
+
+		if (savedScheme !== 'auto') {
+			setScheme(savedScheme)
+		} else {
+			setScheme(systemScheme)
+		}
+	}
+
+	const setScheme = scheme => {
+		document.body.classList.toggle('dark', scheme === 'dark')
+	}
+
+	const saveScheme = scheme => {
+		settingsStore.setColorTheme(scheme)
+	}
+
+	watch(colorTheme, newColorTheme => {
+		if (newColorTheme !== 'auto') {
+			setScheme(newColorTheme)
+			saveScheme(newColorTheme)
+		} else {
+			saveScheme('auto')
+			setScheme(getSystemScheme())
+		}
+	})
+
+	onMounted(() => {
+		setupScheme()
+		setupSwitcher()
+	})
+
+	return { colorTheme, setupScheme, setupSwitcher }
 }
